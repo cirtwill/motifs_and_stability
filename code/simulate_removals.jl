e30c7f845c08d8cca6bff400be3e3ba7ebb82b98
kwootton <lofty.heights@gmail.com>
2021-10-22 18:07:02 +1300

    Code tidied for submission. simulate_initial_foodwebs.jl is the tidied version of test_parameters.jl

--- code/simulate_removals.jl
+++ code/simulate_removals.jl
@@ -1,52 +1,41 @@
 #######################################################
-### Build a food web, simulate the dynamics,
-### remove a species, rinse and repeat
+### 1. Load stable food webs
+### 2. Remove a species, simulate dynamics
+### 3. Repeat for all species
+### 4. Repeat for all webs
 #######################################################
 
-# This code saves the biomasses at each time step after simulating the extinction of a species.
-# Easier to track extinction order in R or python than Julia.
-# Does the extinction threshold work?
+# Run from command line 
+# include(“simulations.jl”) 
 
-# Run from command line (after setting working directory to that below)
-# include(“..\\..\\code\\simulations.jl”) 
-# Or, from Linux:
-# include("..//..//code//simulations.jl")
-
-## Commands for getting set up
-using Pkg
+## Install packages if necessary
 #Pkg.add("BioEnergeticFoodWebs")  # install the package
 #Pkg.add("JSON")
 #Pkg.update()  # check that it's the most up to date one
 
+## Load packages
+using Pkg
 using BioEnergeticFoodWebs  # tell Julia we want to use the package
 using JSON
 using JLD # Need to use this one to pull up the sim
 using CSV
 using DataFrames
-#Do we want to allow rewiring after extinctions? If so which type?
 
-
-# # set working directory
-# cd("C:\\Users\\kewo0002\\gitstuff\\motif_role_stability\\data\\Julia\\")
 
 # initialize params
-reps = 1
-0; # how many replicates do we want?
-Smin=50
-Smax=100
-Cmin=0.02
-Cmax=0.2
+Smin=50;    # minimum number of species
+Smax=100;   # maximum number of species
+Cmin=0.02;	# minimum connectance
+Cmax=0.2;	# maximum connectance
+magn=1;     # how much do we want to decrease biomass by in the disturbance? (1 = full removal)
 
-C = 0.1; # what connectance do we want?
-		 ## NOTE: C gets converted into links for the function "nichemodel()". If in the conversion the value is < 0, it will throw an error
-		 ## Sometimes it can work a bunch of times before we get an error.
+## NOTE: C gets converted into links for the function "nichemodel()". If in the conversion the value is < 0, it will throw an error
+## Sometimes it can work a bunch of times before we get an error.
+
 start = 0; # where should we start our simulations?
-stop = 50; # and how long should they run?
+stop = 50; # and how long should they run (in steps of 10, so total is stop*10)?
 ext_threshold = 0.00001; # what's our threshold for when a species should be considered extinct?
-percent = 10; # What percentage of species can go extinct in a web before we throw it out?
-# For complete removal, magnitude of biomass reduction=1
-magns = collect(0:0.1:1); # Set which magnitudes of disturbances we want to run (for complete removals set to 1)
-trmnt = "bm"; # do we disturb biomass ("bm") or growth rate ("gr")?
+
 
 # Note: names with colons don't play super well with the terminal.
 function save_biomasses(grid,S,C,i,j)
@@ -69,40 +58,39 @@ function check_isolated(network)
 end
 
 # whichsp is column vector of zeros (not disturbed) and ones (disturbed)
-# magn is a scalar of the magnitude of the disturbance
-# trmnt is a string indicating whether it's biomass ("bm") we're decreasing or growth rate ("gr")
-function perturb(whichsp, magn, trmnt, params, B, i, j, S, start, stop, print = true)
+function perturb(whichsp, magn, params, B, i, j, S, start, stop, print = true)
 	# reset biomass
-	if trmnt == "bm"
-		removal_bms = zeros(51,S)
-		removal_bms[1,1:end] = B[end,1:end]
-		bm_dist  = copy(B[size(B)[1],:]);
-		# decrease biomass of species specified in whichsp by magn
-		bm_dist  = bm_dist.*(1 .- magn * whichsp);
-		# Breaking the simulation into smaller chunks so that species which go extinct stay extinct
-		# One "step" in simulate = 10 timesteps. Taking only last row to start the next one.
-		let timestep = 1;
-			while (timestep < 50) # Will be 500 simulated steps total.
-				sim_dist = simulate(params, bm_dist, start = start, stop = 1);
-				final_bms = sim_dist[:B][end,1:end]
-				for q in findall(final_bms .< ext_threshold);
-					final_bms[q]=float(0)
-				end
-				bm_dist = final_bms
-				removal_bms[timestep+1,1:end]=final_bms
-				timestep += 1
+
+	removal_bms = zeros(stop+1,S)   # matrix for output. Rows are timesteps, columns are species
+	removal_bms[1,1:end] = B[end,1:end]  # start biomasses with biomasses from end of initial simulation
+	bm_dist  = copy(B[size(B)[1],:]);   
+	# decrease biomass of species specified in whichsp by magn
+	bm_dist  = bm_dist.*(1 .- magn * whichsp);
+	# Breaking the simulation into smaller chunks so that species which go extinct stay extinct
+	# One "step" in simulate = 10 timesteps. Taking only last row to start the next one.
+	let timestep = 1;
+		while (timestep < stop) # Will be 500 simulated steps total.
+			sim_dist = simulate(params, bm_dist, start = start, stop = 10);   # run the simulation
+			final_bms = sim_dist[:B][end,1:end]                               # what are the biomasses at the end of the simulation?
+			for q in findall(final_bms .< ext_threshold);                     # Find those species that went extinct (below the extinction threshold)
+				final_bms[q]=float(0)                                         # And set them to zero so we don't get zombies
 			end
+			bm_dist = final_bms                                               # Final biomasses from the disturbance become the biomasses we start the next round from
+			removal_bms[timestep+1,1:end]=final_bms
+			timestep += 1
 		end
 	end
 
-	# # how do I reset growth rate?
-	# if trmnt == "gr"
-	# 	bm_dist = copy(sim[:B][size(sim[:B])[1],:])
-	# 	println("WARNING: Can't change growth rate yet")
-	# end
-
 	return removal_bms
 end 
+
+
+# Make directory to save data (may also need to make data/ and networks/)
+try 
+	mkdir("../data/networks/removals")
+catch
+end
+
 
 
 # Run through the species range
@@ -122,33 +110,27 @@ for S in Smin:10:Smax
 		# Run through all the webs
 		for i in 0:4
 			# Load in the initial web
-			B=Matrix(CSV.read("../data/networks/pre_disturbance/$S/$C/initial_B_$i.csv",DataFrame))
-			t=CSV.read("../data/networks/pre_disturbance/$S/$C/initial_t_$i.csv",DataFrame)
-			A=CSV.read("../data/networks/pre_disturbance/$S/$C/initial_net_$i.csv",DataFrame)
+			B=Matrix(CSV.read("../data/networks/pre_disturbance/$S/$C/initial_B_$i.csv",DataFrame))      # biomasses
+			t=CSV.read("../data/networks/pre_disturbance/$S/$C/initial_t_$i.csv",DataFrame)              # 
+			A=CSV.read("../data/networks/pre_disturbance/$S/$C/initial_net_$i.csv",DataFrame)            # network
 
 			# Extract the initial food web, parameters
 			foodweb = Matrix(A)
-			Fwsum = sum(foodweb, dims =2)[1:end,1]
-			verts = Bool[ x > 0 for x in Fwsum]
-			params = model_parameters(foodweb, vertebrates = verts, Z=Float64(3.065))
+			Fwsum = sum(foodweb, dims =2)[1:end,1]                                                       # How many prey does each species have?
+			verts = Bool[ x > 0 for x in Fwsum]                                                          # Set all non-basal species to vertebrates
+			params = model_parameters(foodweb, vertebrates = verts, Z=Float64(3.065))                    # calculate the parameters
 
 			# Biomasses are the last post-equilibrium biomasses
 			bm = B[end,1:end]
 
-			# Don't need to re-simulate.
-
 			# remove each species in turn
-			# start with removing no species
-			nosp = fill(0,S);
 			println("Network ",i)
 			for j in 1:S
-				#println("Species ",j)
-				magn=1
-				whichsp  = copy(nosp);
+				whichsp  = fill(0,S);
 				# set which species to remove
 				whichsp[j] = 1;
 				# run the disturbance, save the biomasses
-				removal_bms = perturb(whichsp, magn, trmnt, params, B, i, j, S, start, stop);
+				removal_bms = perturb(whichsp, magn, params, B, i, j, S, start, stop);
 				save_biomasses(removal_bms,S,C,i,j)
 			end
 
