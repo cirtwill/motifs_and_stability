2811c6ee618ab65d5e03fc7a5bcb75c314903c37
Alyssa Cirtwill <alyssa.cirtwill@gmail.com>
2021-11-15 15:51:35 +0200

    updated Tukey diffs fig

--- code/figure_creation/betadisper_collector.py
+++ code/figure_creation/betadisper_collector.py
@@ -16,57 +16,51 @@ from PyGrace.Styles.el import ElGraph, ElLogColorBar
 
 colors=ColorBrewerScheme('RdYlBu',n=15,reverse=True)  # The blue is very beautiful but maybe harder to see.
 
-def prep_table_anova(dispdir,outfiles,roletype):
-  anovadir={}
-  for fil in outfiles:  # F, p by S and C
-    S=int(fil.split('_')[-2])
-    C=float(fil.split('_')[-1].split('.tsv')[0])
-    f=open(dispdir+roletype+'/betadisper/'+fil,'r')
-    for line in f:
-      if line.split()[0]=='"Groups"':
-        df=line.split()[1]
-        SS=float(line.split()[2])
-        MS=float(line.split()[3])
-        F=float(line.split()[4])
-        p=float(line.split()[5])
-    f.close()
-    anovadir[(S,C)]=((F,p))
+def prep_table_anova(anovadir,anovafile):
+  S=int(anovafile.split('_')[-2])
+  C=float(anovafile.split('_')[-1].split('.tsv')[0])
+  f=open(anovafile,'r')
+  for line in f:
+    if line.split()[0]=='"Groups"':
+      df=line.split()[1]
+      SS=float(line.split()[2])
+      MS=float(line.split()[3])
+      F=float(line.split()[4])
+      p=float(line.split()[5])
+  f.close()
+  anovadir[S][C]=((F,p))
   return anovadir
 
-def prep_table_linmod(dispdir,outfiles,roletype):
-  lmdir={}
-  for fil in outfiles:  # beta(group), p by S and C
-    S=int(fil.split('_')[-2])
-    C=float(fil.split('_')[-1].split('.tsv')[0])
-    f=open(dispdir+roletype+'/linmod/'+fil,'r')
-    for line in f:
-      if line.split()[0]=='"as.numeric(as.character(group))"':
-        beta=float(line.split()[1])
-        pval=float(line.split()[-1])
-    f.close()
-    lmdir[(S,C)]=((beta,pval))
+def prep_table_linmod(lmdir,lmfile):
+  S=int(lmfile.split('_')[-2])
+  C=float(lmfile.split('_')[-1].split('.tsv')[0])
+  f=open(lmfile,'r')
+  for line in f:
+    if line.split()[0]=='"Quantile"':
+      beta=float(line.split()[1])
+      pval=float(line.split()[-1])
+  f.close()
+  lmdir[S][C]=((beta,pval))
   return lmdir
 
 # Think I want to see what the differences are in significant vs. non-significant pairs...
-def prep_table_Tukey(dispdir,outfiles,roletype):
-  Tdir={}
-  for fil in outfiles:  # beta(group), p by S and C
-    S=int(fil.split('_')[-2])
-    C=float(fil.split('_')[-1].split('.tsv')[0])
-    Tdir[(S,C)]={'sig':[],'insig':[]}
-    f=open(dispdir+roletype+'/Tukey/'+fil,'r')
-    for line in f:
-      if line.split()[0]!='"diff"':
-        groups=line.split()[0][1:-1].split('-')
-        diff=float(line.split()[1])
-        lower=float(line.split()[2])
-        upper=float(line.split()[3])
-        p=float(line.split()[4])
-        if p<0.05:
-          Tdir[(S,C)]['sig'].append((groups,diff,p))
-        else:
-          Tdir[(S,C)]['insig'].append((groups,diff,p))
-    f.close()
+def prep_table_Tukey(Tdir,infile):
+  S=int(infile.split('_')[-2])
+  C=float(infile.split('_')[-1].split('.tsv')[0])
+  Tdir[(S,C)]={'sig':[],'insig':[]}
+  f=open(infile,'r')
+  for line in f:
+    if line.split()[0]!='"diff"':
+      groups=line.split()[0][1:-1].split('-')
+      diff=float(line.split()[1])
+      lower=float(line.split()[2])
+      upper=float(line.split()[3])
+      p=float(line.split()[4])
+      if p<0.05:
+        Tdir[(S,C)]['sig'].append((groups,diff,p))
+      else:
+        Tdir[(S,C)]['insig'].append((groups,diff,p))
+  f.close()
 
   return Tdir
 
@@ -75,13 +69,14 @@ def write_supptable(anovadir,lmdir,roletype):
   f.write("&&\\multicolumn{2}{c|}{ANOVA}&\\multicolumn{2}{c||}{Regression}&&&\\multicolumn{2}{c|}{ANOVA}&\\multicolumn{2}{c|}{Regression}\\\\\n")
   f.write("S&C&F&$p$-value&$"+"\\"+"beta$&$p$-value&S&C&F&$p$-value&$"+"\\"+"beta$&$p$-value"+ "\\"+"\\"+"\n")
   f.write('\\hline')
-  for (S,C) in sorted(anovadir):
-    if S<80:
-      f.write(str(S)+'&'+str(C)+'&'+str(round(anovadir[(S,C)][0],2))+'&'+str(round(anovadir[(S,C)][1],3))+'&')
-      f.write(str(lmdir[(S,C)][0])+'&'+str(round(lmdir[(S,C)][1],3))+'&')
-      S2=S+30
-      f.write(str(S2)+'&'+str(C)+'&'+str(round(anovadir[(S2,C)][0],2))+'&'+str(round(anovadir[(S2,C)][1],3))+'&')
-      f.write(str(lmdir[(S2,C)][0])+'&'+str(round(lmdir[(S2,C)][1],3))+'\\'+'\\'+'\n')
+  for S in sorted(anovadir):
+    for C in sorted(anovadir[S]):
+      if S<80:
+        f.write(str(S)+'&'+str(C)+'&'+str(round(anovadir[S][C][0],2))+'&'+str(round(anovadir[S][C][1],3))+'&')
+        f.write(str(lmdir[S][C][0])+'&'+str(round(lmdir[S][C][1],3))+'&')
+        S2=S+30
+        f.write(str(S2)+'&'+str(C)+'&'+str(round(anovadir[S2][C][0],2))+'&'+str(round(anovadir[S2][C][1],3))+'&')
+        f.write(str(lmdir[S2][C][0])+'&'+str(round(lmdir[S2][C][1],3))+'\\'+'\\'+'\n')
   f.close()
 
 def format_graph(graph,key):
@@ -93,14 +88,18 @@ def format_graph(graph,key):
   graph.world.xmin=0
   graph.world.xmax=11
   graph.xaxis.tick.major=2
-  if key=='network':
-    graph.world.ymax=1.75
-    graph.yaxis.tick.major=0.4
+  if key=='Z':
+    graph.world.ymax=2.1
+    graph.yaxis.tick.major=0.5
     graph.yaxis.ticklabel.configure(format='decimal',prec=1,char_size=.5)
-  else:
+  elif key=='freq':
     graph.world.ymax=.12
     graph.yaxis.ticklabel.configure(format='decimal',prec=2,char_size=.5)
     graph.yaxis.tick.major=0.03
+  else:
+    graph.world.ymax=.16
+    graph.yaxis.ticklabel.configure(format='decimal',prec=2,char_size=.5)
+    graph.yaxis.tick.major=0.04
   graph.yaxis.label.configure(text='Difference between groups',char_size=.75,just=2)
   graph.xaxis.ticklabel.configure(char_size=.5,format='decimal',prec=0)
   graph.xaxis.label.configure(text='Difference between deciles',char_size=.75,just=2)
@@ -118,12 +117,12 @@ def populate_graph(Tdict,graph,key):
       gr1=float(g1)
       gr2=float(g2)
       x=max([gr1,gr2])-min([gr1,gr2])
-      sigs.append((10*x-.2+random.uniform(0,0.15),abs(diff)))
+      sigs.append((x-.2+random.uniform(0,0.25),abs(diff)))
     for ([g1,g2],diff,p) in Tdict[(S,C)]['insig']:
       gr1=float(g1)
       gr2=float(g2)
       x=max([gr1,gr2])-min([gr1,gr2])
-      insigs.append((10*x+.2-random.uniform(0,0.15),abs(diff)))
+      insigs.append((x+.2-random.uniform(0,0.25),abs(diff)))
 
   sdat=graph.add_dataset(sigs)
   sdat.line.linestyle=0
@@ -146,7 +145,7 @@ def plot_differences(all_Tukeys):
   grace.add_label_scheme('dummy',['Count','Network normalization','Species normalization'])
   grace.set_label_scheme('dummy')
 
-  for key in sorted(all_Tukeys):
+  for key in ['count','freq','Z']:
     graph=grace.add_graph(Panel)
     graph=format_graph(graph,key)
     graph=populate_graph(all_Tukeys[key],graph,key)
@@ -163,17 +162,22 @@ def plot_differences(all_Tukeys):
 def main():
 
   all_Tukeys={}
-  dispdir='../../data/summaries/extorder_disps/'
+  dispdir='../../data/permanova/'
   for roletype in os.listdir(dispdir): # count, network norm, species norm
-    for testtype in os.listdir(dispdir+roletype): # betadisper (anova), Tukey HSD, linear model (linmod)
-      outfiles=os.listdir(dispdir+roletype+'/'+testtype)
-      if testtype=='betadisper':
-        anovadir=prep_table_anova(dispdir,outfiles,roletype)
-      elif testtype=='linmod':
-        lmdir=prep_table_linmod(dispdir,outfiles,roletype)
-      else:
-        Tukeydir=prep_table_Tukey(dispdir,outfiles,roletype)
+    print roletype
+    anovadir={}
+    lmdir={}
+    Tukeydir={}
+    for S in os.listdir(dispdir+roletype+'/disps/'):
+      anovadir[int(S)]={}
+      lmdir[int(S)]={}
+      for C in os.listdir(dispdir+roletype+'/disps/'+S):
+        anovadir=prep_table_anova(anovadir,dispdir+roletype+'/disps/'+S+'/'+C+'/dispersion_anova_'+S+'_'+C+'.tsv')
+        lmdir=prep_table_linmod(lmdir,dispdir+roletype+'/disps/'+S+'/'+C+'/LM_dispersion_vs_quantile_'+S+'_'+C+'.tsv')
+        Tukeydir=prep_table_Tukey(Tukeydir,dispdir+roletype+'/disps/'+S+'/'+C+'/Tukey_dispersion_vs_quantile_'+S+'_'+C+'.tsv')
+
         all_Tukeys[roletype]=Tukeydir
+
     write_supptable(anovadir,lmdir,roletype)
 
   plot_differences(all_Tukeys)
